3a63dfeca4a4cbd1ce61b3733bb6dd7c4b24b1e6
Volodymyr Smahliuk <volodymyr.smahliuk@alpacked.io>
2022-03-03 12:56:21 +0200

    Avoid misleading in calling send_email function

--- src/inventory/handler.py
+++ src/inventory/handler.py
@@ -1,6 +1,8 @@
 # (c) 2019 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
 # License:
 # This sample code is made available under the MIT-0 license. See the LICENSE file.
+import os
+
 from inventory.readers import AwsConfigInventoryReader
 from inventory.reports import send_email_with_attachment
 from inventory.reports import CreateReportCommandHandler, DeliverReportCommandHandler
@@ -10,7 +12,10 @@ def lambda_handler(event, context):
 
     report_path = CreateReportCommandHandler().execute(inventory)
     report_url = DeliverReportCommandHandler().execute(report_path)
-    send_email_with_attachment(report_path)
+
+    # Won't be executed if RECIPIENT or SENDER empty
+    if os.environ.get("RECIPIENT") and os.environ.get("SENDER"):
+        send_email_with_attachment(report_path)
 
     return {'statusCode': 200,
             'body': {
--- src/inventory/reports.py
+++ src/inventory/reports.py
@@ -7,7 +7,6 @@ from pathlib import PurePath
 import os, os.path
 from typing import List
 import boto3
-import json
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from inventory.mappers import InventoryData
@@ -85,11 +84,6 @@ class DeliverReportCommandHandler():
 
 
 def send_email_with_attachment(file_path):
-    # Won't be executed if RECIPIENT or SENDER empty
-    if not os.environ.get("RECIPIENT") or not os.environ.get("SENDER"):
-        _logger.info(f"No email source or target. Skip")
-        return 0
-
     # Amazon SES Documentation
     # https://docs.aws.amazon.com/ses/latest/dg/send-email-raw.html#send-email-raw-api
 
