049f31da5d652cd9e037293d6d2e4a658de16c4d
Volodymyr Smahliuk <volodymyr.smahliuk@alpacked.io>
2022-03-04 13:15:07 +0200

    Filter updating "BlockDeviceMappings". Don't trigger lambda

--- src/parser/handler.py
+++ src/parser/handler.py
@@ -12,21 +12,29 @@ client = boto3.client('lambda')
 def lambda_handler(event, context):
     _logger.info(event)
     status_code = ''
+    trigger_lambda = False
 
     if not event['invokingEvent']:
         _logger.info('No "invokingEvent". Skip execution.')
     else:
         diff = json.loads(event['invokingEvent'])
         if diff['configurationItemDiff']:
-            _logger.info(f'Triggering {os.environ.get("INVENTORY_FUNCTION_NAME")} lambda')
-            response = client.invoke(
-                FunctionName=os.environ.get('INVENTORY_FUNCTION_NAME'),
-                InvocationType='Event',
-                ClientContext='string',
-                Payload=json.dumps({"TriggeredBy": context.function_name}),
-            )
+            for k,v in diff['configurationItemDiff']['changedProperties'].items():
+                if 'BlockDeviceMappings' in k:
+                    _logger.info(f'The EBS update was marked as spam. Skip execution.')
+                else:
+                    trigger_lambda = True
+            if trigger_lambda:
+                _logger.info(f'Triggering {os.environ.get("INVENTORY_FUNCTION_NAME")} lambda')
+                response = client.invoke(
+                    FunctionName=os.environ.get('INVENTORY_FUNCTION_NAME'),
+                    InvocationType='Event',
+                    ClientContext='string',
+                    Payload=json.dumps({"TriggeredBy": context.function_name}),
+                )
 
-            status_code = response['StatusCode']
+                status_code = response['StatusCode']
+                
         else:
             _logger.info('No "configurationItemDiff". Skip execution.')
 
